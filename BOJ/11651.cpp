09ffccf43a97a8915e3a261023bd045741a87e2d
jiyun-HA <hajy920@gmail.com>
2025-07-28 18:24:14 +0900

    sort 기준 바꾸기
    - bool 함수를 정의해서 세번째 파라미터로 삽입
    - a < b는 오름차순, a > b는 내림차순
    - 정렬 대상이 구조체/pair/string/vector 등이라면 const &으로 호출 부하 개선

---
+++ BOJ/11651.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compareY(const pair<int,int>& a, const pair<int,int>& b) {
+    if (a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
+
+int main() {
+    cin.tie(NULL), cout.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int,int>> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    sort(arr.begin(), arr.end(), compareY);
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i].first << " " << arr[i].second << '\n';
+    }
+    
+    return 0;
+}
