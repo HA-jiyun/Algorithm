7f2a69486a99622979457f0736dd82188d1aebf2
jiyun-HA <hajy920@gmail.com>
2026-02-06 18:39:08 +0900

    백트래킹 DFS - 중복조합

---
+++ BOJ/15652.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 8
+
+int N, M;
+int selected[MAX] = { 0, };
+
+void dfs(int depth, int start) {
+    if (depth == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << selected[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = start; i <= N; ++i) {
+        selected[depth] = i;
+        dfs(depth + 1, i);
+    }
+};
+
+int main() {
+    cin.tie(NULL), cout.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    cin >> N >> M;
+    dfs(0, 1);
+    
+    return 0;
+}
